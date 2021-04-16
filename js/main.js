c79ecc6eb988551ae0b838e1a568d8a389ee30ee
bayuadity <bayuadity@gmail.com>
2021-04-16 13:47:27 +0700

    pertma kali commit di lokal ya

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Bayu Prastyo.", "UI UX Enthusiast."],
       typeSpeed: 100,
       loop: true,
     });
